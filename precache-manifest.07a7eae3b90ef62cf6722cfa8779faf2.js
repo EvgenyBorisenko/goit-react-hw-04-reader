06aa2fecbffe5b6d50dbc1106c97d5a761ed64bc
EvgenyBorisenko <borisenkoevgeny1991@gmail.com>
2019-09-13 17:25:21 +0300

    Updates

---
+++ precache-manifest.07a7eae3b90ef62cf6722cfa8779faf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9467a9fedb3c48d972e073f4bfb5be79",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "786880eb166cb0a956f8",
+    "url": "/goit-react-hw-04-reader/static/css/main.c0f215ae.chunk.css"
+  },
+  {
+    "revision": "f95ed1ab148b4033aaba",
+    "url": "/goit-react-hw-04-reader/static/js/2.61701e76.chunk.js"
+  },
+  {
+    "revision": "786880eb166cb0a956f8",
+    "url": "/goit-react-hw-04-reader/static/js/main.1c49f7d4.chunk.js"
+  },
+  {
+    "revision": "84bf89e08b01550b9b7d",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.860f1cd1.js"
+  }
+]);
